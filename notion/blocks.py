220ecd601f0d4e495306880fc7745d8a9074d65d
Jamie Alexandre <jamalex@gmail.com>
2018-11-26 13:24:09 -0800

    Add "move_to" method on Block, and "id" and "parent" properties.

--- notion/blocks.py
+++ notion/blocks.py
@@ -16,16 +16,30 @@ class Block(object):
     alive = field_map("alive")
 
     def __init__(self, client, block_id, *args, **kwargs):
-        self.client = client
-        self.block_id = extract_id(block_id)
+        self._client = client
+        self._block_id = extract_id(block_id)
+
+    @property
+    def id(self):
+        return self._block_id
 
     @property
     def children(self):
         if not hasattr(self, "_children"):
             children_ids = self.get("content", [])
-            self.client.bulk_update_block_cache(children_ids)
-            self._children = tuple([self.client.get_block(id) for id in children_ids])
+            self._client.bulk_update_block_cache(children_ids)
+            self._children = tuple([self._client.get_block(id) for id in children_ids])
         return self._children
+
+    @property
+    def parent(self):
+        parent_id = self.get("parent_id")
+        parent_table = self.get("parent_table")
+        if not parent_id or parent_table != "block":
+            return None
+        if not hasattr(self, "_parent"):
+            self._parent = self._client.get_block(parent_id)
+        return self._parent
 
     def __str__(self):
         return "type={}".format(self.type)
@@ -37,12 +51,12 @@ class Block(object):
         """
         Update the cached data for this block from the server (data for other blocks may be updated as a side effect).
         """
-        self.client.update_block_cache(self.block_id)
+        self._client.update_block_cache(self.id)
 
     def _get_block_data(self):
-        if self.block_id not in self.client.block_cache:
+        if self.id not in self._client.block_cache:
             self.refresh()
-        return self.client.block_cache[self.block_id]
+        return self._client.block_cache[self.id]
 
     def get(self, path=[], default=None, refresh=False):
         """
@@ -67,9 +81,71 @@ class Block(object):
         Set a specific `value` (under the specific `path`) on the block's data structure on the server.
         If `refresh` is set to True, we refresh the data cache from the server after sending the update.
         """
-        self.client.submit_transaction(build_operation(id=self.block_id, path=path, args=value))
+        self._client.submit_transaction(build_operation(id=self.id, path=path, args=value))
         if refresh:
             self.refresh()
+
+    def move_to(self, target_block, position="last-child"):
+        assert isinstance(target_block, Block), "target_block must be an instance of Block or one of its subclasses"
+        assert position in ["first-child", "last-child", "before", "after"]
+
+        old_parent_id = self.get("parent_id")
+
+        if "child" in position:
+            new_parent_id = target_block.id
+            new_parent_table = "block"
+        else:
+            new_parent_id = target_block.get("parent_id")
+            new_parent_table = target_block.get("parent_table")
+
+        if position in ["first-child", "before"]:
+            list_command = "listBefore"
+        else:
+            list_command = "listAfter"
+
+        list_args = {"id": self.id}
+        if position in ["before", "after"]:
+            list_args[position] = target_block.id
+
+        with self._client.as_atomic_transaction():
+            # Temporarily mark the block being moved as inactive
+            self._client.submit_transaction(
+                build_operation(
+                    id=self.id,
+                    path=[],
+                    args={"alive": False},
+                    command="update",
+                )
+            )
+            # Remove the block's ID from the "content" list of its current parent
+            self._client.submit_transaction(
+                build_operation(
+                    id=old_parent_id,
+                    path=["content"],
+                    args={"id": self.id},
+                    command="listRemove",
+                )
+            )
+            # Set the parent_id of the moving block to the new parent, and mark it as active again
+            self._client.submit_transaction(
+                build_operation(
+                    id=self.id,
+                    path=[],
+                    args={"alive": True, "parent_id": new_parent_id, "parent_table": new_parent_table},
+                    command="update",
+                )
+            )
+            # Add the moving block's ID to the "content" list of the new parent
+            self._client.submit_transaction(
+                build_operation(
+                    id=new_parent_id,
+                    path=["content"],
+                    args=list_args,
+                    command=list_command,
+                )
+            )
+            # update the local block cache to reflect the updates
+            self._client.bulk_update_block_cache(block_ids=[self.id, self.get("parent_id"), target_block.id, target_block.get("parent_id")])
 
 
 class BasicBlock(Block):
@@ -84,10 +160,10 @@ class BasicBlock(Block):
         assert new_type in BLOCK_TYPES and issubclass(BLOCK_TYPES[new_type], BasicBlock), \
             "Target type must correspond to a subclass of BasicBlock"
         self.type = new_type
-        return self.client.get_block(self.block_id)
+        return self._client.get_block(self.id)
 
     def __str__(self):
-        return "title={}".format(repr(self.title))
+        return "id={}, title={}".format(self.id, repr(self.title))
 
 
 class TodoBlock(BasicBlock):
@@ -106,11 +182,11 @@ class MediaBlock(Block):
     caption = property_map("caption")
 
     def __str__(self):
-        return "caption={}".format(repr(self.caption))
+        return "id={}, caption={}".format(self.id, repr(self.caption))
 
 
 class ImageBlock(MediaBlock):
-    
+
     width = field_map("format.block_width")
 
 
