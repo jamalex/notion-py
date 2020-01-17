58be04af563fa2a4da5379db729b14bacc33684b
Cobertos / Peter <me+git@cobertos.com>
2020-01-17 02:30:43 -0500

    Remove level, smaller function names for duplicates, correct audio, video, and img tags, render markdown, moved styling to stub stylesheet

--- notion/renderer.py
+++ notion/renderer.py
@@ -1,7 +1,9 @@
 import markdown2
 import requests
 import dominate
+import threading
 from dominate.tags import *
+from dominate.util import raw
 
 from .block import *
 
@@ -17,17 +19,48 @@ class BaseRenderer(object):
 		self.start_block = start_block
 
 	def render(self):
+		pass
+
+	def render_block(self, block):
+		pass
+
+def renderMD(mdStr):
+	"""
+	Render the markdown string to HTML, wrapped with dominate "raw" so Dominate
+	renders it straight to HTML
+	"""
+	return raw(markdown2.markdown(mdStr))
+
+class BaseHTMLRenderer(BaseRenderer):
+	"""
+	BaseRenderer for HTML output, uses [Dominate](https://github.com/Knio/dominate)
+	internally for generating HTML output
+	Each token rendering method should create a dominate tag and it automatically
+	gets added to the parent context (because of the with statement). If you return
+	a given tag, it will be used as the parent container for all rendered children
+	"""
+
+	def __init__(self, start_block):
+		self.start_block = start_block
+
+	def render(self):
 		with div() as d:
 			self.render_block(self.start_block)
-		return d
+		return "".join(str(d) for d in d.children) #Return the array of Dominate dom_tags as strings
 
-	def calculate_child_indent(self, block):
-		if block.type == "page":
-			return 0
-		else:
-			return 1
+	def get_parent_element(self):
+		"""
+		Returns the current parent Dominate element (uses Dominate internals)
+		https://github.com/Knio/dominate/issues/123
+		"""
+		def _get_thread_context():
+			context = [threading.current_thread()]
+			# if greenlet:
+			# 	context.append(greenlet.getcurrent())
+			# 	return hash(tuple(context))
+		return dom_tag._with_contexts[_get_thread_context()]
 
-	def render_block(self, block, level=0):
+	def render_block(self, block):
 		assert isinstance(block, Block)
 		type_renderer = getattr(self, "handle_" + block._type, None)
 		if not callable(type_renderer):
@@ -35,173 +68,158 @@ class BaseRenderer(object):
 				type_renderer = self.handle_default
 			else:
 				raise Exception("No handler for block type '{}'.".format(block._type))
-		#Render ourselves to an HTML element and then add all our children to it
-		selfEl = type_renderer(block, level=level)
-		with selfEl if isinstance(selfEl, dominate.dom_tag.dom_tag) else nullcontext():
-			for child in block.children:
-				self.render_block(child, level=level+self.calculate_child_indent(block))
-		return selfEl
+		#Render ourselves to an HTML element which automatically gets added to the parent
+		#context with Dominate
+		selfEl = type_renderer(block)
+		if block.children:
+			#If we have children, we need to add them to ourself (if we returned a tag
+			#to add to) or make a new container for the children
+			with selfEl or div(_class='children-list'):
+				for child in block.children:
+					self.render_block(child)
 
+	def handle_default(self, block):
+		p(renderMD(block.title))
 
-bookmark_template = """
-<div>
-   <div style="display: flex;">
-      <a target="_blank" rel="noopener noreferrer" href="{link}" style="display: block; color: inherit; text-decoration: none; flex-grow: 1; min-width: 0px;">
-         <div role="button" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; width: 100%; display: flex; flex-wrap: wrap-reverse; align-items: stretch; text-align: left; overflow: hidden; border: 1px solid rgba(55, 53, 47, 0.16); border-radius: 3px; position: relative; color: inherit; fill: inherit;">
-            <div style="flex: 4 1 180px; min-height: 60px; padding: 12px 14px 14px; overflow: hidden; text-align: left;">
-               <div style="font-size: 14px; line-height: 20px; color: rgb(55, 53, 47); white-space: nowrap; overflow: hidden; text-overflow: ellipsis; margin-bottom: 2px;">{title}</div>
-               <div style="font-size: 12px; line-height: 16px; color: rgba(55, 53, 47, 0.6); height: 32px; overflow: hidden;">{description}</div>
-               <div style="display: flex; margin-top: 6px;">
-                  <img src="{icon}" style="width: 16px; height: 16px; min-width: 16px; margin-right: 6px;">
-                  <div style="font-size: 12px; line-height: 16px; color: rgb(55, 53, 47); white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{link}/div>
-               </div>
-            </div>
-            <div style="flex: 1 1 180px; min-height: 80px; display: block; position: relative;">
-               <div style="position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px;">
-                  <div style="width: 100%; height: 100%;"><img src="{cover}" style="display: block; object-fit: cover; border-radius: 1px; width: 100%; height: 100%;"></div>
-               </div>
-            </div>
-         </div>
-      </a>
-   </div>
-</div>
-"""
-
-callout_template = """
-<div style="padding: 16px 16px 16px 12px; display: flex; width: 100%; border-radius: 3px; border-width: 1px; border-style: solid; border-color: transparent; background: rgba(235, 236, 237, 0.3);">
-   <div>
-      <div role="button" style="user-select: none; cursor: pointer; display: flex; align-items: center; justify-content: center; height: 24px; width: 24px; border-radius: 3px; flex-shrink: 0;">
-         <div style="display: flex; align-items: center; justify-content: center; height: 24px; width: 24px;">
-            <div style="height: 16.8px; width: 16.8px; font-size: 16.8px; line-height: 1.1; margin-left: 0px; color: black;">{icon}</div>
-         </div>
-      </div>
-   </div>
-   <div style="max-width: 100%; width: 100%; white-space: pre-wrap; word-break: break-word; caret-color: rgb(55, 53, 47); margin-left: 8px;">{title}</div>
-</div>
-"""
-
-class BaseHTMLRenderer(BaseRenderer):
-	def handle_default(self, block, level=0):
-		p(block.title)
-
-	def handle_divider(self, block, level=0):
+	def handle_divider(self, block):
 		hr()
 
-	def handle_column_list(self, block, level=0):
+	def handle_column_list(self, block):
 		return div(style='display: flex;', _class='column-list')
 
-	def handle_column(self, block, level=0):
+	def handle_column(self, block):
 		return div(_class='column')
 
-	def handle_to_do(self, block, level=0):
+	def handle_to_do(self, block):
 		id = f'chk_{block.id}'
-		input(type='checkbox', id=id, checked=block.checked, title=block.title).add(
-			label(_for=id)).add(br())
+		input(label(_for=id), type='checkbox', id=id, checked=block.checked, title=block.title)
 
-	def handle_code(self, block, level=0):
-		code(block.title)
+	def handle_code(self, block):
+		#TODO: Do we want this to support Markdown? I think there's a notion-py
+		#change that might affect this... (the unstyled-title or whatever)
+		pre(code(block.title))
 
-	def handle_factory(self, block, level=0):
+	def handle_factory(self, block):
 		pass
 
-	def handle_header(self, block, level=0):
-		h2(block.title)
+	def handle_header(self, block):
+		h2(renderMD(block.title))
 
-	def handle_sub_header(self, block, level=0):
-		h3(block.title)
+	def handle_sub_header(self, block):
+		h3(renderMD(block.title))
 
-	def handle_sub_sub_header(self, block, level=0):
-		h4(block.title)
+	def handle_sub_sub_header(self, block):
+		h4(renderMD(block.title))
 
-	def handle_page(self, block, level=0):
-		h1(block.title)
+	def handle_page(self, block):
+		h1(renderMD(block.title))
 
-	def handle_bulleted_list(self, block, level=0):
-		ctx = next(dom_tag._with_contexts.values())
-		with ctx.children[-1] if isinstance(ctx.children[-1], ul) else ul():
-			li(block.title)
+	def handle_bulleted_list(self, block):
+		parent = self.get_parent_element()
+		#print(parent)
+		lastChild = parent.children[-1] if hasattr(parent, 'children') else None
+		#Open a new ul if the last child was not a ul
+		with lastChild if lastChild and isinstance(lastChild, ul) else ul():
+			li(renderMD(block.title))
 
-	def handle_numbered_list(self, block, level=0):
-		ctx = next(dom_tag._with_contexts.values())
-		with ctx.children[-1] if isinstance(ctx.children[-1], ol) else ol():
-			li(block.title)
+	def handle_numbered_list(self, block):
+		parent = self.get_parent_element()
+		#print(parent)
+		lastChild = parent.children[-1] if hasattr(parent, 'children') else None
+		#Open a new ul if the last child was not a ol
+		with lastChild if lastChild and isinstance(lastChild, ol) else ol():
+			li(renderMD(block.title))
 
-	def handle_toggle(self, block, level=0):
-		details(summary(block.title))
+	def handle_toggle(self, block):
+		details(summary(renderMD(block.title)))
 
-	def handle_quote(self, block, level=0):
-		blockquote(block.title)
+	def handle_quote(self, block):
+		blockquote(renderMD(block.title))
 
-	def handle_text(self, block, level=0):
-		return self.handle_default(block=block, level=level)
+	handle_text = handle_default
 
-	def handle_equation(self, block, level=0):
+	def handle_equation(self, block):
 		p(img(src=f'https://chart.googleapis.com/chart?cht=tx&chl={block.latex}'))
 
-	def handle_embed(self, block, level=0):
+	def handle_embed(self, block):
 		iframe(src=block.display_source or block.source, frameborder=0,
 			sandbox='allow-scripts allow-popups allow-forms allow-same-origin',
 			allowfullscreen='')
 
-	def handle_video(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	def handle_video(self, block):
+		#TODO, this won't work if there's no file extension, we might have
+		#to query and get the MIME type...
+		src = block.display_source or block.source
+		srcType = src.split('.')[-1]
+		video(source(src=src, type=f"video/{srcType}"), controls=True)
 
-	def handle_file(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	handle_file = handle_embed
+	handle_pdf = handle_embed
 
-	def handle_audio(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	def handle_audio(self, block):
+		audio(src=block.display_source or block.source, controls=True)
 
-	def handle_pdf(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	def handle_image(self, block):
+		attrs = {}
+		if block.caption: # Add the alt attribute if there's a caption
+			attrs['alt'] = block.caption
+		img(src=block.display_source or block.source, **attrs)
 
-	def handle_image(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	def handle_bookmark(self, block):
+		#return bookmark_template.format(link=, title=block.title, description=block.description, icon=block.bookmark_icon, cover=block.bookmark_cover)
+		#It's just a social share card for the website we're bookmarking
+		return a(href="block.link")
 
-	def handle_bookmark(self, block, level=0):
-		return bookmark_template.format(link=block.link, title=block.title, description=block.description, icon=block.bookmark_icon, cover=block.bookmark_cover)
-
-	def handle_link_to_collection(self, block, level=0):
+	def handle_link_to_collection(self, block):
 		a(href=f'https://www.notion.so/{block.id.replace("-", "")}')
 
-	def handle_breadcrumb(self, block, level=0):
-		p(block.title)
+	def handle_breadcrumb(self, block):
+		p(renderMD(block.title))
 
-	def handle_collection_view(self, block, level=0):
+	def handle_collection_view(self, block):
 		a(href=f'https://www.notion.so/{block.id.replace("-", "")}')
 
-	def handle_collection_view_page(self, block, level=0):
+	def handle_collection_view_page(self, block):
 		a(href=f'https://www.notion.so/{block.id.replace("-", "")}')
 
-	def handle_framer(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	handle_framer = handle_embed
 
-	def handle_tweet(self, block, level=0):
+	def handle_tweet(self, block):
 		return requests.get("https://publish.twitter.com/oembed?url=" + block.source).json()["html"]
 
-	def handle_gist(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	handle_gist = handle_embed
+	handle_drive = handle_embed
+	handle_figma = handle_embed
+	handle_loom = handle_embed
+	handle_typeform = handle_embed
+	handle_codepen = handle_embed
+	handle_maps = handle_embed
+	handle_invision = handle_embed
 
-	def handle_drive(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
+	def handle_callout(self, block):
+		div( \
+			div(block.icon, _class="icon") + div(renderMD(block.title), _class="text"), \
+		_class="callout")
 
-	def handle_figma(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
-
-	def handle_loom(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
-
-	def handle_typeform(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
-
-	def handle_codepen(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
-
-	def handle_maps(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
-
-	def handle_invision(self, block, level=0):
-		return self.handle_embed(block=block, level=level)
-
-	def handle_callout(self, block, level=0):
-		return callout_template.format(icon=block.icon, title=markdown2.markdown(block.title))
+#This is the minimal css stylesheet to apply to get
+#decent lookint output, it won't make it look exactly like Notion.so
+#but will have the same basic structure
+"""
+.children-list {
+	margin-left: cRems(20px);
+}
+.column-list {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.callout {
+	display: flex;
+}
+.callout > .icon {
+	flex: 0 1 40px;
+}
+.callout > .text {
+	flex: 1 1 auto;
+}
+"""
