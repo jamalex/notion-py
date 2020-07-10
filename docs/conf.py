b975c64305540239159cf34e402c821640170be2
Artur Tamborski <tamborskiartur@gmail.com>
2020-07-10 13:01:10 +0200

    docs: add workflow, init dir, create makefile workflow (#34)
    * docs: add workflow, init dir, create makefile workflow
    
    * reformat code
    
    * Fix Makefile.test target, Publish Docs only on push to master
    
    * actions: use makefile in every workflow
    
    * run commands in makefile via python -m

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from datetime import date
+
+# for import below
+sys.path.insert(0, os.path.abspath("../"))
+
+from notion import __name__, __author__, __version__
+
+project = __name__
+author = __author__
+release = __version__
+copyright = f"{date.today().year}, {__author__}"
+
+html_theme = "alabaster"
+
+extensions = [
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autodoc",
+]
+
+napoleon_include_init_with_doc = True
+napoleon_google_docstring = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+html_baseurl = "asd.pl"
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+Welcome to notion-py's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   :hidden:
+
+   modules <source/modules.rst>
+   notion <source/notion.rst>
+
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
